e3eb0370ecd76cc60a43968dc4c593acd8547ca6
Jacob Albright <jacob.albright23@gmail.com>
2018-11-02 21:10:59 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-navbar></app-navbar>
+    <app-side-nav :show-if="showSideNav"></app-side-nav>
+    <div ref="main">
+      <button @click="showSideNav = !showSideNav">Toggle Sidenav</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import appSideNav from './components/SideNav.vue'
+import appNavbar from './components/Navbar.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    appSideNav,
+    appNavbar
+  },
+  data() {
+    return {
+      showSideNav: false,
+    }
   }
 }
 </script>
@@ -23,6 +32,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
+
+/* #main {
+  transition: margin-left .5s;
+  padding: 16px;
+} */
+
+
 </style>
---
+++ src/components/SideNav.vue
@@ -0,0 +1,61 @@
+<template>
+  <transition name="sidenav">
+    <div v-if="showIf" id="sidenav">
+      <router-link href="">Hello</router-link>
+      <router-link href="">Hi</router-link>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    showIf: Boolean,
+  }
+}
+</script>
+
+<style scoped>
+
+#sidenav {
+  height: 100%;
+  width: 200px;
+  position: fixed;
+  z-index: 1;
+  background-color: #222;
+  overflow-x: hidden;
+  padding-top: 60px;
+}
+
+#sidenav router-link {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 20px;
+  color: #818181;
+  display: block;
+}
+
+#sidenav router-link:hover {
+  color: #f1f1f1;
+  cursor: pointer;
+}
+
+
+.sidenav-enter-active {
+  animation: sidenav-show .5s ease-in-out;
+}
+
+.sidenav-leave-active {
+  animation: sidenav-show .5s reverse ease-in-out;
+}
+
+@keyframes sidenav-show {
+  0% {
+    left: -200px;
+  }
+  100% {
+    left: 0px;
+  }
+}
+
+</style>
